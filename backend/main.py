f5936a05c6564ccf9a64035deb53a6a28174afef
godsmith <j.godsmith@proton.me>
2025-01-19 13:46:56 -0500

    syncing work to github
    delete this commit later

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI, UploadFile
+from pathlib import Path
+
+UPLOAD_DIR = Path() / "uploads"
+UPLOAD_DIR.mkdir(exist_ok=True)
 
 app = FastAPI()
 
 
 @app.post("/uploadimage/")
-async def create_upload_image(image: UploadFile):
-    return {"filename": image.filename}
+async def create_upload_image(image_upload: UploadFile):
+    if image_upload.size == 0:
+        return {"message": "File is empty"}
 
-# if __name__ == "__main__":
-#     main()
+    image = await image_upload.read()
+    save_path = UPLOAD_DIR / image_upload.filename
+    with open(save_path, "wb") as file:
+        file.write(image)
+    return {"filename": image_upload.filename}
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
